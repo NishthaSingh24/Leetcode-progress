074091b34b5e2b4dfc5f47257ad52250d8494db7
Nishtha Singh <233193@kit.ac.in>
2026-07-11 21:07:43 +0530

    Time: 3 ms (41.57%), Space: 43.3 MB (20.08%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] count = new int[26];
+        
+        for(int i=0; i< s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for(int j=0; j< t.length(); j++){
+            count[t.charAt(j) - 'a']--;
+        }
+
+        for(int i=0; i<count.length; i++){
+            if(count[i] < 0){
+                return (char)(i + 'a'); 
+            }
+        }
+        return '\0';
+    }
+}
